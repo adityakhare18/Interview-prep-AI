9086a36c7d1a0b67d39145870bc5cce0dbc8cf4c
adityakhare18 <khareaditya0105@gmail.com>
2025-06-04 22:11:19 +0530

    fix: update CORS origin to production endpoint

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://interview-prep-ai-1-91as.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
